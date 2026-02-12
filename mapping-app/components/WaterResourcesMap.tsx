fead705a8261d414dc9505984ea88fe4b485686b
N01715807 <lpc20231016@gmail.com>
2026-02-12 08:06:06 -0500

    change map

--- mapping-app/components/WaterResourcesMap.tsx
+++ mapping-app/components/WaterResourcesMap.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import { GoogleMap, Marker, InfoWindow, useLoadScript } from "@react-google-maps/api";
 import { useEffect, useState } from "react";
 
 export default function WaterResourcesMap() {
@@ -9,6 +9,7 @@ export default function WaterResourcesMap() {
   });
 
   const [data, setData] = useState<any[]>([]);
+  const [selected, setSelected] = useState<any | null>(null);
 
   useEffect(() => {
     fetch("/api/water-resources")
@@ -20,8 +21,8 @@ export default function WaterResourcesMap() {
 
   return (
     <GoogleMap
-      center={{ lat: 52.1332, lng: -106.67 }}   // Saskatoon
-      zoom={12}
+      center={{ lat: 52.9, lng: -106.0 }} 
+      zoom={6}
       mapContainerStyle={{ width: "100%", height: "80vh" }}
     >
       {data.map((r) => (
@@ -31,8 +32,24 @@ export default function WaterResourcesMap() {
             lat: Number(r.latitude),
             lng: Number(r.longitude),
           }}
+          onClick={() => setSelected(r)}
         />
       ))}
+      {selected && (
+        <InfoWindow
+        position={{
+          lat: Number(selected.latitude),
+          lng: Number(selected.longitude),
+        }}
+        onCloseClick={() => setSelected(null)}
+        >
+          <div>
+            <strong>{selected.name}</strong>
+            <p>Type: {selected.type}</p>
+            <p>Status: {selected.status}</p>
+          </div>
+          </InfoWindow>
+        )}
     </GoogleMap>
   );
 }
